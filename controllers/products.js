f85ecda4da6587a847beb6823d7ea74ef70fe1d2
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-11-29 14:53:30 -0300

    Numeric Filters addition

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require("../models/product");
 
 // '/'
 const getAllProducts = async (req, res) => {
-  const { name, featured, company, sort, fields } = req.query;
+  const { name, featured, company, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (name) {
@@ -28,6 +28,34 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
   res.status(200).json({ total: products.length, products });

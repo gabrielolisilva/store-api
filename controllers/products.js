fa795cab63fdbe1c7a4caba4cee8a36007194f13
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-11-29 10:10:18 -0300

    Filter addition

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,21 @@ const Product = require("../models/product");
 
 // '/'
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { name, featured, company } = req.query;
+  const queryObject = {};
+
+  if (name) {
+    queryObject.name = name; //parâmetro name recebe variável name
+  }
+  if (featured) {
+    queryObject.featured = featured === "1" ? 1 : 0;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  console.log(queryObject);
+  const productsFilter = await Product.find(queryObject);
+  res.status(200).json({ total: productsFilter.length, productsFilter });
 };
 
 // '/static'
